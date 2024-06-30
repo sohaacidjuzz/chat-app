c576b1fb5987913ddbebeae2711f94ba96ca61bc
sohamsinha <visitsohamhere@gmail.com>
2024-07-01 00:04:37 +0530

    remove unneccesary ode

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from "styled-components"
 import {BiPowerOff} from "react-icons/bi"
-import axios from 'axios'
 function LogOut() {
     const navigate = useNavigate();
     const handleClick = async () => {
